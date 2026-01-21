d313cd2bf81659fbf9ea36fc263ad4df0992c22a
Tenghoit Kouch <tenghoitkouch@gmail.com>
2026-01-21 11:57:23 -0500

    load json + length

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+
+  const [sites, setSites] = useState([])
+
+  useEffect(() => {
+    async function fetchSites() {
+        const url = "/BoyleSites.json";
+        const response = await fetch(url);
+        if(response.ok){          
+          const result = await response.json();
+          setSites(result)
+        }
+    };
+    fetchSites();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p># Sites: {sites.length}</p>
     </div>
   );
 }
